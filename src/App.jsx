068f06b37c10ccbfbde4ba639af6e21e22856c3c
V Anissankar <uarduino123@gmail.com>
2025-12-08 10:08:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
 import Hero from "./components/sections/Hero";
-import Skills from "./components/sections/Skills";
+import Skills from "./components/sections/skills";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
 import EscapeModal from "./components/common/EscapeModal";
